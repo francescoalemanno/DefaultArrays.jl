fcbe6ce1542dfac95e0c09877523405ebcc8a56d
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-01 16:45:17 +0100

    proper scalar indexing, more tests

--- src/DefaultArrays.jl
+++ src/DefaultArrays.jl
@@ -2,6 +2,7 @@ module DefaultArrays
     import Base.size
     import Base.getindex
     import Base.setindex!
+    import Base.@propagate_inbounds
     export DefaultArray,eachnondefault
     struct DefaultArray{T,N} <: AbstractArray{T,N}
         default::T
@@ -9,7 +10,7 @@ module DefaultArrays
         elements::Dict{NTuple{N,Int},T}
     end
 
-    Base.@propagate_inbounds function DefaultArray(default::T, size::NTuple{N,Int})  where {T,N}
+    @propagate_inbounds function DefaultArray(default::T, size::NTuple{N,Int})  where {T,N}
         DefaultArray(default, size, Dict{NTuple{N,Int},T}())
     end
 
@@ -18,32 +19,34 @@ module DefaultArrays
         ret.=A
         ret
     end
-    function DefaultArray(A::AbstractArray) 
+    function DefaultArray(A::AbstractArray)
         DefaultArray(zero(eltype(A)),A)
     end
 
 
-    Base.size(A::DefaultArray) = A.size
+    size(A::DefaultArray) = A.size
 
 
     @inline function checkbounds(A::DefaultArray,I)
-        if length(I) != length(A.size) || (!all(1 .<= I .<= A.size)) 
+        if length(I) != length(A.size) || (!all(1 .<= I .<= A.size))
             throw(BoundsError)
         end
     end
-    #@inline function getindex(A::DefaultArray,i::Int) #FIXME
-    #    I=CartesianIndices(A)[i]
-    #    A[I]
-    #end
-    @inline function Base.getindex(A::DefaultArray, I::Int...) 
+
+    @inline getindex(A::DefaultArray{T,1}, i::Int) where T =  _cartesian_getindex(A, i)
+    @inline getindex(A::DefaultArray{T,N}, i::Int) where {T,N} =  _scalar_getindex(A, i)
+    @inline getindex(A::DefaultArray, I::Int...) =  _cartesian_getindex(A, I...)
+
+    @inline function _cartesian_getindex(A::DefaultArray, I::Int...)
         @boundscheck checkbounds(A,I)
         get(A.elements, I, A.default)
     end
-    #@inline function setindex!(A::DefaultArray,v,i::Int) #FIXME
-    #    I=CartesianIndices(A)[i]
-    #    A[I]=v
-    #end
-    @inline function Base.setindex!(A::DefaultArray, v, I::Int...) 
+    @inline function _scalar_getindex(A::DefaultArray,i::Int)
+        I=Tuple(CartesianIndices(A)[i])
+        _cartesian_getindex(A, I...)
+    end
+
+    @inline function _cartesian_setindex!(A::DefaultArray, v, I::Int...)
         @boundscheck checkbounds(A,I)
         if v == A.default
             haskey(A.elements,I) && delete!(A.elements,I)
@@ -51,6 +54,14 @@ module DefaultArrays
         end
         A.elements[I] = v
     end
+    @inline function _scalar_setindex!(A::DefaultArray,v,i::Int)
+        I=Tuple(CartesianIndices(A)[i])
+        _cartesian_setindex!(A, v, I...)
+    end
+    @inline setindex!(A::DefaultArray{T,1},v, i::Int) where T =  _cartesian_setindex!(A,v, i)
+    @inline setindex!(A::DefaultArray{T,N},v, i::Int) where {T,N} =  _scalar_setindex!(A,v, i)
+    @inline setindex!(A::DefaultArray,v, I::Int...) =  _cartesian_setindex!(A,v, I...)
+
 
     function eachnondefault(A::DefaultArray)
         (CartesianIndex(x) for x in keys(A.elements) if A.elements[x]!=A.default)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,12 @@
 using Test
 using DefaultArrays
-A=DefaultArray(Inf,(2,2))
+A=DefaultArray(Inf,(2,3))
 A[1,2]=2
 @test maximum(A)==Inf
 @test minimum(A)==2
-
-
+@test A[3]==A[1,2]
+A[4]=3.0
+@test A[4]==A[2,2]
+B=DefaultArray(Inf,(3,))
+B[3]=2
+@test B[3]==2

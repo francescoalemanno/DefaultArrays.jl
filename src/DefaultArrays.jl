cdcab52e2e18e1c0312ec3e071f1355bf78dd5d3
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-03 08:45:29 +0100

    added broadcasting, improved tests

--- src/DefaultArrays.jl
+++ src/DefaultArrays.jl
@@ -9,11 +9,15 @@ module DefaultArrays
         size::NTuple{N,Int}
         elements::Dict{NTuple{N,Int},T}
     end
+    import Base.similar
+
 
     @propagate_inbounds function DefaultArray(default::T, size::NTuple{N,Int})  where {T,N}
         DefaultArray(default, size, Dict{NTuple{N,Int},T}())
     end
-
+    function DefaultArray(default,size::Int...)
+        DefaultArray(default, size)
+    end
     function DefaultArray(default::T, A::AbstractArray{T,N}) where {T,N}
         ret=DefaultArray(default,size(A))
         ret.=A
@@ -22,10 +26,14 @@ module DefaultArrays
     function DefaultArray(A::AbstractArray)
         DefaultArray(zero(eltype(A)),A)
     end
+    function DefaultArray(A::DefaultArray)
+        DefaultArray(A.default,A)
+    end
 
 
     size(A::DefaultArray) = A.size
 
+    similar(A::DefaultArray, ::Type{T}, dims::Dims) where {T} = DefaultArray(convert(T,A.default), dims)
 
     @inline function checkbounds(A::DefaultArray,I)
         if length(I) != length(A.size) || (!all(1 .<= I .<= A.size))
@@ -48,11 +56,12 @@ module DefaultArrays
 
     @inline function _cartesian_setindex!(A::DefaultArray, v, I::Int...)
         @boundscheck checkbounds(A,I)
-        if v == A.default
-            haskey(A.elements,I) && delete!(A.elements,I)
-            return A[I...]
+        if v == A.default && haskey(A.elements,I)
+            delete!(A.elements,I)
+        else
+            A.elements[I] = v
         end
-        A.elements[I] = v
+        v
     end
     @inline function _scalar_setindex!(A::DefaultArray,v,i::Int)
         I=Tuple(CartesianIndices(A)[i])
@@ -67,4 +76,21 @@ module DefaultArrays
         (CartesianIndex(x) for x in keys(A.elements) if A.elements[x]!=A.default)
     end
 
+    import Base.BroadcastStyle
+    BroadcastStyle(::Type{<:DefaultArray{T}}) where {T} = Broadcast.ArrayStyle{DefaultArray{T}}()
+    function similar(bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{DefaultArray{T}}}, ::Type{ElType}) where {ElType,T}
+        da = find_defaultarray(bc)
+        va,vb=promote(da.default,zero(ElType))
+        if typeof(va)==ElType
+            return DefaultArray(va,similar(Array{ElType}, axes(bc)))
+        end
+        return DefaultArray(zero(ElType),similar(Array{ElType}, axes(bc)))
+    end
+
+
+    find_defaultarray(bc::Base.Broadcast.Broadcasted) = find_defaultarray(bc.args)
+    find_defaultarray(args::Tuple) = find_defaultarray(find_defaultarray(args[1]), Base.tail(args))
+    find_defaultarray(x) = x
+    find_defaultarray(a::DefaultArray, rest) = a
+    find_defaultarray(::Any, rest) = find_defaultarray(rest)
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,29 @@
 using Test
 using DefaultArrays
-A=DefaultArray(Inf,(2,3))
-A[1,2]=2
-@test maximum(A)==Inf
-@test minimum(A)==2
-@test A[3]==A[1,2]
-A[4]=3.0
-@test A[4]==A[2,2]
-B=DefaultArray(Inf,(3,))
-B[3]=2
-@test B[3]==2
+
+@testset "Basic Tests" begin
+
+    A=DefaultArray(Inf,(2,3))
+    A[1,2]=2
+    @test maximum(A)==Inf
+    @test minimum(A)==2
+    @test A[3]==A[1,2]
+    A[4]=3.0
+    @test A[4]==A[2,2]
+
+    B=DefaultArray(Inf,(3,))
+    B[3]=2
+    @test B[3]==2
+end
+
+@testset "similar(..) Tests + broadcasting" begin
+    M=similar(DefaultArray(1.0),Float64,2,2)
+
+    @test typeof(M)==DefaultArray{Float64,2}
+    @test all(@inferred  M.==1.0)
+    M[1,2]=0.5
+    @test typeof(@inferred M.+2)==DefaultArray{Float64,2}
+    M.=M.+M'
+    @test all(M.==[2.0 1.5; 1.5 2.0])
+    @test typeof(M)==DefaultArray{Float64,2}
+end
